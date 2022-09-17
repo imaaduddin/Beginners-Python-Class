35f084ca98993abecf1ba4cd978789b6515fbd13
Imaad Uddin <imaaduddin@icloud.com>
2022-09-17 13:18:22 -0500

    comments

---
+++ Lecture 2/comments.py
@@ -0,0 +1,9 @@
+# This is a comment
+# Comments are used using the hashtag (#) symbol 
+
+print('Hello world!')
+
+print('My name is Imaad!')
+
+
+print("hfuhfuhfuhfuhfuf")
--- Lecture 2/print-statement.py
+++ Lecture 2/print-statement.py
@@ -1,8 +1,19 @@
-print('Hello World!')
-print('Imaad Uddin')
-print(13, "Hello")
+# print('Hello World!')
+# print('Imaad Uddin')
+# print(13, "Hello")
 
-print("   /|")
-print("  / |")
-print(" /  |")
-print("/___|")
+# print("   /|")
+# print("  / |")
+# print(" /  |")
+# print("/___|")
+
+
+from tkinter.tix import Tree
+
+
+print('Imaad')
+
+print("They're going to the mall.")
+
+print(1, 2, "hello", True)
+
