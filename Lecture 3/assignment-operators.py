38eee7d241b93ed1fad03a4380fe030565841440
Imaad Uddin <imaaduddin@icloud.com>
2022-09-22 15:35:04 -0500

    lecture 3

---
+++ Lecture 3/assignment-operators.py
@@ -0,0 +1,39 @@
+# Addition 
+num1 = 5
+num1 += 3
+print(num1)
+# Above code is equivalent to:
+num2 = 5 + 3
+print(num2)
+
+# Subtraction
+num1 = 10
+num1 -= 5
+print(num1)
+# Above code is equivalent to:
+num2 = 10 - 5
+print(num2)
+
+# Multiplication 
+num1 = 7
+num1 *= 2
+print(num1)
+# Above code is equivalent to:
+num2 = 7 * 2
+print(num2)
+
+# Division 
+num1 = 12 
+num1 /= 3
+print(num1)
+# Above code is equivalent to:
+num2 = 12 / 3
+print(num2)
+
+# Exponent (Power)
+num1 = 4
+num1 **= 2
+print(num1)
+# Above code is equivalent to:
+num2 = 4 ** 2
+print(num2)
