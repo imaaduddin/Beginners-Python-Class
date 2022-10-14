e799f2050394be1dca2653cd8dacf13b53374d68
Imaad Uddin <imaaduddin@icloud.com>
2022-10-14 12:38:38 -0500

    python lists

---
+++ Lecture 6/list-access.py
@@ -0,0 +1,13 @@
+sports_list = ['football', 'basketball', 'baseball', 'soccer', 'tennis', 'volleyball']
+
+print(sports_list[3]) # grabs an item at the specified element 
+
+print(sports_list[0]) # gets the first item in the list 
+
+print(sports_list[-1]) # gets the last item in the list 
+
+print(sports_list[:3]) # gets items from the end till the one on the third index
+
+print(sports_list[2:]) # prints item from the second index till the end
+
+print(sports_list{::-1}) # reverses the list
---
+++ Lecture 6/lists.py
@@ -0,0 +1,13 @@
+# Lists are used to store multiple items in a single variable.
+
+"""
+Lists are one of 4 built-in data types in Python used to store
+collections of data, the other 3 are Tuple, Set, and Dictionary. Each
+has a different properties and usages. 
+"""
+
+sports_list = ['football', 'basketball', 'baseball', 'soccer', 'tennis', 'volleyball'] # this is a list 
+
+print(sports_list[2]) # this prints out baseball
+
+print(len(sports_list)) # prints out the length of the list
