e0924218562d829df1948bb6f2676d13125ec01d
Imaad Uddin <imaaduddin@icloud.com>
2022-10-17 15:06:09 -0500

    Lecture 6

---
+++ Lecture 6/list-methods.py
@@ -0,0 +1,17 @@
+# Add something to a list append()
+# The append() will add whatever you'd like to the end of a list
+
+# fruits_list = ['orange', 'watermelon', 'grapes', 'mango']
+# fruits_list.append('pineapple')
+# print(fruits_list)
+
+# Insert at a specific index we use the insert() method
+# fruits_list = ['orange', 'watermelon', 'grapes', 'mango']
+# fruits_list.insert(2, 'apple')
+# print(fruits_list)
+
+# To combine a list we can use + or the extend() method
+list1 = ['PlayStation', 'Xbox']
+list2 = ['Nintendo', 'PC']
+list1.extend(list2)
+print(list1)
