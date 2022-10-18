34875a4fe0149715665adad2679362b2cb63e4bb
Imaad Uddin <imaaduddin@icloud.com>
2022-10-18 13:29:06 -0500

    Lecture 6

--- Lecture 6/list-methods.py
+++ Lecture 6/list-methods.py
@@ -11,7 +11,22 @@
 # print(fruits_list)
 
 # To combine a list we can use + or the extend() method
-list1 = ['PlayStation', 'Xbox']
-list2 = ['Nintendo', 'PC']
-list1.extend(list2)
-print(list1)
+# list1 = ['PlayStation', 'Xbox']
+# list2 = ['Nintendo', 'PC']
+# list1.extend(list2)
+# print(list1)
+
+# To remove an item from a list we remove() and put whatever item we would like removed inside the parenthesis 
+# superheros = ['Spider-Man', 'Batman', 'Iron Man', 'Captain America', 'Super Man']
+# superheros.remove('Captain America')
+# print(superheros)
+
+
+# To sort a list we use the sort() method
+numbers = [6, 2, 10, 3, 1, 13, 5, 18]
+# numbers.sort()
+# print(numbers)
+
+# To sort in descending order we would do this
+numbers.sort(reverse=True)
+print(numbers)
