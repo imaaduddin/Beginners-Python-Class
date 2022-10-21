8c752263726c865eca30ca24711c5d589c745288
Imaad Uddin <imaaduddin@icloud.com>
2022-10-20 19:41:55 -0500

    Lecture 6

--- Lecture 6/tuples.py
+++ Lecture 6/tuples.py
@@ -1,3 +1,18 @@
 # Tuples 
 # Tuples are ordered, unchangeable, and allow duplicate values
 
+tuple1 = ('Imaad', 40, True, 'male')
+tuple2 = ('Texas', 'Houston')
+
+print(len(tuple1)) # to get the length
+
+print(type(tuple))
+
+print(tuple1[1]) #to get the first element, same indexing as lists
+print(tuple1 + tuple2) # to concatenate tuples
+
+# Tuple Assignments 
+julia = ("Julia", "Roberts", 1967, "Duplicity", 2009, "Actress", "Atlanta, Georgia")
+(name, surname, bYear, movie, mYear, profession, birthplace) = julia
+
+print(julia)
